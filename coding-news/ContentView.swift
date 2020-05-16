372b0a1099e2b57451019daeaaa9a1587011eda6
Erwan Leboucher <erwan.leboucher@barzahlen.de>
2020-05-16 12:29:42 +0200

    Initial Commit

---
+++ coding-news/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coding-news
+//
+//  Created by Erwan Leboucher on 16/05/2020.
+//  Copyright © 2020 Erwan Leboucher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
